5a3a43187ba82140e86cdfce21d91cc132a2fd5b
Illia Hladush <illia.gladush@gmail.com>
2020-10-20 20:21:14 -0400

    Changing directory structure

---
+++ Algorithms/Sorting/BubbleSort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] arr = new int[] {1,6,54,3,6,87,2};
+            Array.Sort(arr);
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
